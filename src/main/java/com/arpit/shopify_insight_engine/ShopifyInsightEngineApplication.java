ccd425ee516ad81645c4bd495db29d68070c7d27
Arpit Pathak <arpitpathak@Arpits-MacBook-Air.local>
2025-11-05 06:08:50 +0530

    Initial commit

---
+++ src/main/java/com/arpit/shopify_insight_engine/ShopifyInsightEngineApplication.java
@@ -0,0 +1,13 @@
+package com.arpit.shopify_insight_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyInsightEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifyInsightEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arpit/shopify_insight_engine/ShopifyInsightEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpit.shopify_insight_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyInsightEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
